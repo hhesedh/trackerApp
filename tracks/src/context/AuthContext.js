543cb3ae007db9f35056a6bf9b12a3d5bb68738e
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-11 16:18:36 -0300

    Automatic SignIn

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -8,17 +8,27 @@ const authreducer = (state, action) => {
 		case 'add_error':
 			return { ...state, errorMessage: action.payload };
 		case 'signin':
-            return { ...state, token: action.payload };
-        case 'clear_error_message':
-            return {...state, errorMessage: ''}
+			return { ...state, token: action.payload };
+		case 'clear_error_message':
+			return { ...state, errorMessage: '' };
 		default:
 			return state;
 	}
 };
 
+const tryLocalSigin = dispatch => async () => {
+	const token = await AsyncStorage.getItem('token');
+	if (token) {
+		dispatch({ type: 'signin', payload: token });
+		navigate('TrackList');
+	} else {
+		navigate('Signup');
+	}
+};
+
 const clearErrorMessage = dispatch => () => {
-    dispatch({type: 'clear_error_message'})
-}
+	dispatch({ type: 'clear_error_message' });
+};
 
 const signup = dispatch => async ({ email, password }, callback) => {
 	// make api request to sign up with that email and password
@@ -47,8 +57,8 @@ const signin = dispatch => async ({ email, password }) => {
 	try {
 		const response = await trackerApi.post('/signin', { email, password });
 		await AsyncStorage.setItem('token', response.data.token);
-        dispatch({ type: 'signin', payload: response.data.token });
-        navigate('TrackList');
+		dispatch({ type: 'signin', payload: response.data.token });
+		navigate('TrackList');
 	} catch (err) {
 		dispatch({
 			type: 'add_error',
@@ -64,6 +74,6 @@ const signout = dispatch => {
 };
 export const { Provider, Context } = createDataContext(
 	authreducer,
-	{ signup, signout, signin, clearErrorMessage },
+	{ signup, signout, signin, clearErrorMessage, tryLocalSigin },
 	{ token: null, errorMessage: '' }
 );
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import { Context as AuthContext } from '../context/AuthContext';
@@ -7,7 +7,13 @@ import NavLink from '../components/NavLink';
 import { NavigationEvents } from 'react-navigation';
 
 const SignupScreen = ({ navigation }) => {
-	const { state, signup, clearErrorMessage } = useContext(AuthContext);
+	const { state, signup, clearErrorMessage, tryLocalSigin } = useContext(
+		AuthContext
+	);
+
+	useEffect(() => {
+		tryLocalSigin();
+	}, []);
 	return (
 		<View style={styles.container}>
 			<NavigationEvents onWillFocus={clearErrorMessage} />
